21d18b16e49ef05f08ad9640c3a29826947451ae
ustahot <info.iluha@gmail.com>
2024-03-12 20:26:18 +0300

    step_001

--- ReturnOperation.php
+++ ReturnOperation.php
@@ -7,14 +7,55 @@ class TsReturnOperation extends ReferencesOperation
     public const TYPE_NEW    = 1;
     public const TYPE_CHANGE = 2;
 
+
+    private array $data;
+    private Seller $reseller;
+    private Client $client;
+    private int $notificationType;
+
+
     /**
+     * @return void
      * @throws \Exception
      */
-    public function doOperation(): void
+    public function validateRequestAndFillProperties()
     {
-        $data = (array)$this->getRequest('data');
-        $resellerId = $data['resellerId'];
-        $notificationType = (int)$data['notificationType'];
+        $data = $this->getRequestParam('data');
+
+        // Этой проверки не было, но она не помешает
+        if (!isset($data)){
+            throw new \Exception('Empty data');
+        }
+
+        $this->data = (array) $data;
+
+        if (!isset($this->data['resellerId'])) {
+            throw new \Exception('Empty resellerId', 400);
+        }
+
+        $this->reseller = Seller::getById($this->data['resellerId']);
+
+        if (!isset($this->reseller)){
+            throw new \Exception('Seller not found!', 400);
+        }
+
+        if (!isset($this->data['notificationType'])) {
+            throw new \Exception('Empty notificationType', 400);
+        }
+
+        $this->notificationType = (int)$this->data['notificationType'];
+
+    }
+
+
+    /**
+     * Учитывая, что метод не статичны, подразумеваю, что гдето снаружи будет создаваться экземпляр этого класса
+     * @return array
+     * @throws \Exception
+     */
+    public function doOperation(): array
+    {
+
         $result = [
             'notificationEmployeeByEmail' => false,
             'notificationClientByEmail'   => false,
@@ -24,57 +65,73 @@ class TsReturnOperation extends ReferencesOperation
             ],
         ];
 
-        if (empty((int)$resellerId)) {
+
+        // Странно, что эта проверка кладет сообщение в массив возвращаемого значения,
+        // а остальные проверки генерят исключение. Как то концепция не единая. Но учитывая, что вызывающий
+        // код может быть заточен именно под такое поведение, я такое поведение и оаставлю. Хотя,
+        // красивее было бы сделать однотипно и реализовать эту проверку вместе с остальными
+        // проверками в validateContextAndFillProperties()
+        // Ну и раз проверять, то $data['resellerId'], а не $resellerId
+        if (empty($data['resellerId'])) {
             $result['notificationClientBySms']['message'] = 'Empty resellerId';
             return $result;
         }
 
-        if (empty((int)$notificationType)) {
-            throw new \Exception('Empty notificationType', 400);
-        }
+        $this->validateRequestAndFillProperties();
 
-        $reseller = Seller::getById((int)$resellerId);
-        if ($reseller === null) {
-            throw new \Exception('Seller not found!', 400);
-        }
 
-        $client = Contractor::getById((int)$data['clientId']);
-        if ($client === null || $client->type !== Contractor::TYPE_CUSTOMER || $client->Seller->id !== $resellerId) {
-            throw new \Exception('сlient not found!', 400);
+        // Тут бы желательно реализовать класс Client, унаследованный от класса Contractor
+        // а класс Contractor сделать абстрактным. А метод getById() в следующей строке брать от класса Client
+        $client = Client::getById((int)$data['clientId']);
+        // $client === null || можно убрать из условия ниже, т.к. этот фрагмент будет всегда false
+        if ($client->type !== Contractor::TYPE_CUSTOMER || $client->Seller->id !== $this->reseller->id) {
+            throw new \Exception('client not found!', 400);
         }
 
         $cFullName = $client->getFullName();
+        // в условии ниже есть смысл проверять не результат метода, а переменную $cFullName
+        // и похоже, это избыточная проверка, т.к. при верной реализации класса Contractor, в полное имя попадет,
+        // как минимум, id
         if (empty($client->getFullName())) {
             $cFullName = $client->name;
         }
 
-        $cr = Employee::getById((int)$data['creatorId']);
-        if ($cr === null) {
+        // ниже не информативное имя переменной. Перименовываю
+        $creator = Employee::getById((int)$data['creatorId']);
+        // Условие ниже избыточно, т.к. всегда будет false
+        if ($creator === null) {
             throw new \Exception('Creator not found!', 400);
         }
 
-        $et = Employee::getById((int)$data['expertId']);
-        if ($et === null) {
+        // ниже не информативное имя переменной. Перименовываю
+        $expert = Employee::getById((int)$data['expertId']);
+        // Условие ниже избыточно, т.к. всегда будет false
+        if ($expert === null) {
             throw new \Exception('Expert not found!', 400);
         }
 
         $differences = '';
         if ($notificationType === self::TYPE_NEW) {
-            $differences = __('NewPositionAdded', null, $resellerId);
+            // ниже используется неопределенная функция (предположим, что потом будет какая то реализация).
+            // И не рекомендуется пользовательские функции так называть,
+            // т.к. такой префикс используется для магических методов
+            $differences = __('NewPositionAdded', null, $this->reseller->id);
         } elseif ($notificationType === self::TYPE_CHANGE && !empty($data['differences'])) {
+            $statusFrom = Status::getById((int)$data['differences']['from']); // Не делаю валидацию на на наличие объекта, т.к. допускаю, что она предполагается в реализации функции __
+            $statusTo = Status::getById((int)$data['differences']['to']);
             $differences = __('PositionStatusHasChanged', [
-                    'FROM' => Status::getName((int)$data['differences']['from']),
-                    'TO'   => Status::getName((int)$data['differences']['to']),
-                ], $resellerId);
+                    'FROM' => $statusFrom->getName(),
+                    'TO'   => $statusTo->getName(),
+                ], $this->reseller->id);
         }
 
         $templateData = [
             'COMPLAINT_ID'       => (int)$data['complaintId'],
             'COMPLAINT_NUMBER'   => (string)$data['complaintNumber'],
             'CREATOR_ID'         => (int)$data['creatorId'],
-            'CREATOR_NAME'       => $cr->getFullName(),
+            'CREATOR_NAME'       => $creator->getFullName(),
             'EXPERT_ID'          => (int)$data['expertId'],
-            'EXPERT_NAME'        => $et->getFullName(),
+            'EXPERT_NAME'        => $expert->getFullName(),
             'CLIENT_ID'          => (int)$data['clientId'],
             'CLIENT_NAME'        => $cFullName,
             'CONSUMPTION_ID'     => (int)$data['consumptionId'],
@@ -91,19 +148,24 @@ class TsReturnOperation extends ReferencesOperation
             }
         }
 
-        $emailFrom = getResellerEmailFrom($resellerId);
+        $emailFrom = getResellerEmailFrom($this->reseller->id);
         // Получаем email сотрудников из настроек
-        $emails = getEmailsByPermit($resellerId, 'tsGoodsReturn');
+        $emails = getEmailsByPermit($this->reseller->id, 'tsGoodsReturn');
         if (!empty($emailFrom) && count($emails) > 0) {
             foreach ($emails as $email) {
+                // Отсутствует реализация класса MessagesClient
+                // Ну и для соблюдения приципа Единой отвественности, есть смысл отправку
+                // сообщения реализовывать не в классе самогО сообщения, а в отдельном транспортном классе.
+                // Сообщение можно реализовать, как модель, а Транспортный класс, как сервис
+                // Не естественно выглядит, когда сообщение отправляет само себя
                 MessagesClient::sendMessage([
                     0 => [ // MessageTypes::EMAIL
                            'emailFrom' => $emailFrom,
                            'emailTo'   => $email,
-                           'subject'   => __('complaintEmployeeEmailSubject', $templateData, $resellerId),
-                           'message'   => __('complaintEmployeeEmailBody', $templateData, $resellerId),
+                           'subject'   => __('complaintEmployeeEmailSubject', $templateData, $this->reseller->id),
+                           'message'   => __('complaintEmployeeEmailBody', $templateData, $this->reseller->id),
                     ],
-                ], $resellerId, NotificationEvents::CHANGE_RETURN_STATUS);
+                ], $this->reseller->id, NotificationEvents::CHANGE_RETURN_STATUS);
                 $result['notificationEmployeeByEmail'] = true;
 
             }
@@ -116,15 +178,18 @@ class TsReturnOperation extends ReferencesOperation
                     0 => [ // MessageTypes::EMAIL
                            'emailFrom' => $emailFrom,
                            'emailTo'   => $client->email,
-                           'subject'   => __('complaintClientEmailSubject', $templateData, $resellerId),
-                           'message'   => __('complaintClientEmailBody', $templateData, $resellerId),
+                           'subject'   => __('complaintClientEmailSubject', $templateData, $this->reseller->id),
+                           'message'   => __('complaintClientEmailBody', $templateData, $this->reseller->id),
                     ],
-                ], $resellerId, $client->id, NotificationEvents::CHANGE_RETURN_STATUS, (int)$data['differences']['to']);
+                ], $this->reseller->id, $client->id, NotificationEvents::CHANGE_RETURN_STATUS, (int)$data['differences']['to']);
                 $result['notificationClientByEmail'] = true;
             }
 
             if (!empty($client->mobile)) {
-                $res = NotificationManager::send($resellerId, $client->id, NotificationEvents::CHANGE_RETURN_STATUS, (int)$data['differences']['to'], $templateData, $error);
+                // строка ниже слишком длинная. Есть смысл ее отформатировать
+                // Используется переменная $error, кторая нигде не определена
+                // Отсутствует реализация класса NotificationManager
+                $res = NotificationManager::send($this->reseller->id, $client->id, NotificationEvents::CHANGE_RETURN_STATUS, (int)$data['differences']['to'], $templateData, $error);
                 if ($res) {
                     $result['notificationClientBySms']['isSent'] = true;
                 }
--- others.php
+++ others.php
@@ -1,26 +1,95 @@
 <?php
 
+/**
+ * 1) Классы не следует помещать все в один файл. Точнее сказать - в одном файле должно быть не более одного класса.
+ *      Имена файла и класса должны совпадать. Разумеется - это не требование языка, а рекомендация PSR
+ * 2) Если мы реализуем получение значений свойств объекта геттером, то нет смысла делать эти свойства публичными
+ * 3) Желательно сопровождать методы PHPDoc-блоками. Я ниже их добавлю
+ * 4) Пространство имен и расположение файлов проекта не коррелируется с PSR-4.
+ *      Как следствие, автозагрузку если и можно реализовать, то тоже без соотвествия PSR
+ */
+
 namespace NW\WebService\References\Operations\Notification;
 
 /**
+ * 1) Свойство Seller описано в PHPDoc, но не описано в самом классе. К тому же начинается с заглавной буквы,
+ * что противоречит PSR-2. Заглавную букву оставляю, т.к. какой то внешний код может быть уже заточен под это
+ * Также свойство $Seller оставляю публичным по тем же причинам
+ * 2) Предположим, что используем версию php 7+, или 8+, тогда есть смысл тепизировать свойства при объявлении,
+ * что сделает код строже, понятнее и снизит вероятность ошибки при выполнении скрипта
+ * 3) Свойства id, $type и $name оставляю так же публичными, для совместимости с вероятным кодом заточенным под
+ * такую реализацию.
+ * 4) Геттеры не делаю, т.к. свойства публмчные
+ * 5) Этот класс напрашивается стать абстрактным, но не делаю его таковым, т.к. возможно существует код, который
+ *  создает объекты от этого класса
+ *
  * @property Seller $Seller
  */
 class Contractor
 {
     const TYPE_CUSTOMER = 0;
-    public $id;
-    public $type;
-    public $name;
 
-    public static function getById(int $resellerId): self
+    public Seller $Seller;
+    public int $id;
+    public int $type;
+    public string $name;
+
+
+    /**
+     * @param array $attributes
+     */
+    public function __construct(array $attributes)
     {
-        return new self($resellerId); // fakes the getById method
+        $this->id = $attributes['id'];
+        $this->type = $attributes['type'];
+        $this->name = $attributes['name'];
     }
 
+
+    /**
+     * По всей видимости, тут имитация получения экземпляра класса, по его id. Текущий метод лучше сделать статичным.
+     * Полагаю, не исключено, что может возвращать null, если экземпляра с таким id не будет найдено
+     *
+     * @param int $contractorId
+     * @return static|null
+     *
+     * входящий параметр лучше назвать $contractorId.
+     * Как выяснилось позже, желательно реализовать класс Client дочерний от текущего. А текущий сделать абстрактным
+     */
+    public static function getById(int $contractorId): ?self
+    {
+
+        /**
+         * @var array $attributes
+         */
+
+        // Тут предполагается какая то реализация поиска данных по id, харркатерных для экземпляра текущего класса,
+        // а также заполнение массива аттрибутов характерных для этого экземпляра. id экземпляра так же поместим
+        // в массив атрибутов
+
+        return new static($attributes); // fakes the getById method
+    }
+
+
+    /**
+     * 1) Нет единой концепции. Где-то используются геттеры, а где-то свойтва читаются напрямую из оъекта.
+     *  Если делаем уклон в сторону целостности объекта, то стоит закрыть свойства видимостью private и использовать
+     *  геттеры и сеттеры. Так же, если свойство инициализируется единожды и впоследствии не меняется, то
+     *  в случае php 8.1+ можно использовать атрибут readonly, область видимости сделать protected или public и
+     *  отказаться от гетеров, для удобства
+     *
+     *
+     * @return string
+     */
     public function getFullName(): string
     {
         return $this->name . ' ' . $this->id;
     }
+}
+
+
+class Client extends Contractor
+{
 }
 
 class Seller extends Contractor
@@ -31,32 +100,108 @@ class Employee extends Contractor
 {
 }
 
+
+/**
+ * Тут было бы правильно реализовать этот класс в виде модели данных
+ */
 class Status
 {
+
+    // меняю область видимости, т.к. работаем через геттер
     public $id, $name;
 
-    public static function getName(int $id): string
+
+    /**
+     * Возвращает статус по его id
+     * @param int $id
+     * @return Status|null
+     */
+    public static function getById(int $id)
     {
-        $a = [
+
+        $item = self::all();
+
+        if (!isset($item[$id])){
+
+            return null;
+        }
+
+        $instance = new self();
+        $instance->id = $id;
+        $instance->name = $item[$id];
+
+        return $instance;
+    }
+
+
+    /**
+     * Возвращает все статусы в виде массива
+     * @return string[]
+     */
+    public static function all()
+    {
+
+        return [
             0 => 'Completed',
             1 => 'Pending',
             2 => 'Rejected',
         ];
-
-        return $a[$id];
     }
+
+    /**
+     * Учитывая комментарий к классу, геттер надо делать не статичным (переделываю),
+     * а обычным методом и применять его от объекта (в контексте this).
+     * К тому же тут еще возможна ошибка несуществующего инлекса массива. Никакой валидации не реализовано.
+     * При реализации класса, как модель данных, этот вопрос вообще не возникал бы. Объект либо есть, либо его нет
+     *
+     * @param int $id
+     * @return string
+     */
+    public function getName(): string
+    {
+
+        return $this->name;
+    }
+
 }
+
 
 abstract class ReferencesOperation
 {
     abstract public function doOperation(): array;
 
-    public function getRequest($pName)
+    private $request;
+
+
+    /**
+     *  Реализовано для устранения некрасивого проектирования. Например, для оправдания нестатичности метода getRequestParam()
+     */
+    public function __construct()
     {
-        return $_REQUEST[$pName];
+        $this->request = $_REQUEST;
+    }
+
+
+    /**
+     * Тоже странная реализация. Используем нестатичный геттер для получения сущности, не относящейся к классу.
+     *  Меняю архитектуру. Метод оставляю не статичным, но атрибуты будут браться не из $_REQUEST, а из свойства $request,
+     *  в которое ранее будетпомещен $_REQUEST
+     * @param $pName
+     * @return mixed
+     */
+    public function getRequestParam($pName)
+    {
+
+        return $this->request[$pName] ?? null;
     }
 }
 
+
+/**
+ * Опять отсутствие единой концепции. Вперемешку ООП и процедурный стили
+ *
+ * @return string
+ */
 function getResellerEmailFrom()
 {
     return 'contractor@example.com';
@@ -67,6 +212,8 @@ function getEmailsByPermit($resellerId, $event)
     // fakes the method
     return ['someemeil@example.com', 'someemeil2@example.com'];
 }
+
+
 
 class NotificationEvents
 {
